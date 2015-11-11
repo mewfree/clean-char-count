feca7dfbad1f1b0edd7a333ef2f2e77eedb30698
mewfree <damien.gonot@gmail.com>
2015-11-10 22:41:38 -0500

    Let's change port to 80 for prod

--- app.js
+++ app.js
@@ -24,7 +24,7 @@ app.get('/', function (req, res) {
 //});
 
 // Start server
-var server = app.listen(3000, function () {
+var server = app.listen(80, function () {
 
   var host = server.address().address;
   var port = server.address().port;
