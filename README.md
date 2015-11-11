e345c1e2d27144af1963cc757ffa9411a93d5db6
mewfree <damien.gonot@gmail.com>
2015-11-10 22:34:35 -0500

    Initial setup

---
+++ README.md
@@ -0,0 +1,5 @@
+# Clean character counter
+I always need to use a character counter for work and none have:
+* A clean design
+* No ads
+* Warnings for popular character limitations
---
+++ app.js
@@ -0,0 +1,34 @@
+var express = require('express')
+  , stylus = require('stylus')
+  , _ = require('lodash');
+
+// Express config
+var app = express();
+
+app.set('views', './views');
+app.set('view engine', 'jade');
+
+app.use(stylus.middleware ({
+  src: __dirname + '/views',
+  dest: __dirname + '/public'
+}));
+app.use(express.static(__dirname + '/public'));
+
+// Routes
+app.get('/', function (req, res) {
+  res.render('index');
+});
+
+//app.get('/toast', function(req, res) {
+  //res.send('TOAST');
+//});
+
+// Start server
+var server = app.listen(3000, function () {
+
+  var host = server.address().address;
+  var port = server.address().port;
+
+  console.log('App listening at http://%s:%s', host, port);
+
+});
